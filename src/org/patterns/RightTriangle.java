a475e76da0b9c0da6e6f7a88e879118bb81f5870
Vishvajeet Angane <vishvajeetangane@gmail.com>
2026-09-03 00:26:35 +0530

    Add Right Triangle Star Pattern

---
+++ src/org/patterns/RightTriangle.java
@@ -0,0 +1,25 @@
+/*
+      *
+    * *
+  * * *
+* * * *
+
+*/
+
+
+package org.patterns;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        for(int i = 0; i < 4; i++){
+            for(int j = 3; j > i; j--){
+                System.out.print(" ");
+            }
+
+            for(int k = 0; k <= i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
